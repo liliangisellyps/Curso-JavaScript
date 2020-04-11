2bbfea61b270c0b2567e45db52e5cdcecbad94ac
liliangisellyps <51760530+liliangisellyps@users.noreply.github.com>
2020-04-11 20:48:43 -0300

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,6 @@
+"use strict";
+
+//6° exercicio : Converta o seguinte trecho de código utilizando Template Literals:
+var usuario = 'Diego';
+var idade = 23;
+console.log("O usu\xE1rio ".concat(usuario, "  possui  ").concat(idade, "  anos"));
---
+++ main.js
@@ -0,0 +1,4 @@
+//6° exercicio : Converta o seguinte trecho de código utilizando Template Literals:
+const usuario = 'Diego';
+const idade = 23;
+console.log(`O usuário ${usuario}  possui  ${idade}  anos`);
